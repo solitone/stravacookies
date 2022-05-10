487e94af8c40e764c9f446c0f20a166e64afac3a
solitone <solitone@mail.com>
2022-05-10 16:58:52 +0200

    First version of the package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stravacookies',
+      version='1.0',
+      description='A small package providing methods for retrieving Strava\'s HTTP cookies needed to download Strava\'s global heatmap in high resolution.',
+      url='https://github.com/solitone/stravacookies',
+      author='solitone',
+      author_email='solitone@mail.com',
+      license='GPL 3.0',
+      packages=['stravacookies'],
+     )
